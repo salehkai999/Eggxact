a3b8b2ab17793e36a352cd3a4703ff7afe410566
Korey Lo <koreylo9@gmail.com>
2021-05-19 13:14:49 -0700

    Created edit profile layout and able to hide/show profile/editProfile

--- app/src/main/java/com/se491/eggxact/ui/landingpage/ProfileFragment.java
+++ app/src/main/java/com/se491/eggxact/ui/landingpage/ProfileFragment.java
@@ -2,11 +2,15 @@ package com.se491.eggxact.ui.landingpage;
 
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.se491.eggxact.R;
 
@@ -16,6 +20,12 @@ import com.se491.eggxact.R;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+
+    ConstraintLayout profileLayout;
+    ConstraintLayout editProfileLayout;
+    ScrollView editScroll;
+    Button editProfileBtn;
+    Button saveChanges;
 
 
 
@@ -36,7 +46,29 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View fragmentView = inflater.inflate(R.layout.fragment_profile, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        profileLayout = fragmentView.findViewById(R.id.profileLayout);
+        editProfileLayout = fragmentView.findViewById(R.id.editProfileLayout);
+        editScroll = fragmentView.findViewById(R.id.editScroll);
+        editScroll.setVisibility(View.INVISIBLE);
+        editProfileBtn = fragmentView.findViewById(R.id.edit_profile);
+        editProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                profileLayout.setVisibility(View.INVISIBLE);
+                editScroll.setVisibility(View.VISIBLE);
+            }
+        });
+
+        saveChanges = fragmentView.findViewById(R.id.save_editProfile);
+        saveChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                profileLayout.setVisibility(View.VISIBLE);
+                editScroll.setVisibility(View.INVISIBLE);
+            }
+        });
+        return fragmentView;
     }
 }
