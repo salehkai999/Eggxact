8d03cc165ec6d2a71f32217d6f3cca8939a66aa5
saleh <saleh.mk96@gmail.com>
2021-06-02 21:02:29 -0500

    Filters initial commit.

---
+++ app/src/main/java/com/se491/eggxact/ui/filtering/FiltersActivity.java
@@ -0,0 +1,16 @@
+package com.se491.eggxact.ui.filtering;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.se491.eggxact.R;
+
+public class FiltersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filters);
+    }
+}
